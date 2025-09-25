91322b7a19f5fba8d1cfeef3d7ed27ba88854caa
ohholylymyr-bit <ohholylymyr@gmail.com>
2025-09-25 14:59:35 +0300

    Create Firebaseconfig.js

---
+++ Firebaseconfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyATO0586cwwqxxV0h1t16WrKfLgbXsy0-g",
+  authDomain: "henrybankki-ja-hamster-exc.firebaseapp.com",
+  projectId: "henrybankki-ja-hamster-exc",
+  storageBucket: "henrybankki-ja-hamster-exc.firebasestorage.app",
+  messagingSenderId: "931698320471",
+  appId: "1:931698320471:web:b7736abed3c840f9e3ef12",
+  measurementId: "G-W603PB8QNK"
+};
